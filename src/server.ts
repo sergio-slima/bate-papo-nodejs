464c43b7cab59ca7be3011ff5e7b2282d1a12989
unknown <sergio_silvalima@hotmail.com>
2021-04-28 15:21:22 -0300

    Instalando pacote database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,44 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * 
+ */
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá NLW#5"
+    });
+});
+
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Usuário salvo com sucesso!"
+    })
+});
+
 app.listen(3333, () => console.log("Server is running on port 3333"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // yarn add express
 // yarn add @types/express -D
@@ -10,4 +46,7 @@ app.listen(3333, () => console.log("Server is running on port 3333"));
 // yarn tsc --init
 // colocar false no strict do tsconfig.json
 // yarn add ts-node-dev -D
-// yarn dev
+//
+// yarn dev //iniciar servidor
+//
+// yarn add typeorm reflect-metadata sqlite3
